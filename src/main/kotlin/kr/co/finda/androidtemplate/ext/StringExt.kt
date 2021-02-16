fb75dc8713d9fe1a46bfaf483c56abe9db5c98da
kimdohun <kimdohun0104@gmail.com>
2021-02-16 09:28:15 +0900

    decapitalizeWithUnderBar 버그 수정

--- src/main/kotlin/kr/co/finda/androidtemplate/ext/StringExt.kt
+++ src/main/kotlin/kr/co/finda/androidtemplate/ext/StringExt.kt
@@ -1,8 +1,9 @@
 package kr.co.finda.androidtemplate.ext
 
 fun String.decapitalizeWithUnderBar(): String {
-    return this.split("^[A-Z]$".toRegex())
+    return this.split("(?=[A-Z])".toRegex())
         .joinToString("_") { it.decapitalize() }
+        .replaceFirst("_", "")
 }
 
 fun String.replaceAll(oldValue: String, newValue: String?): String {
