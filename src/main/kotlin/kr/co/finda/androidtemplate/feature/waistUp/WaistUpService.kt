bee9a8e1d1ba17fd27e8dbfb27efabc7917ab952
kimdohun <kimdohun0104@gmail.com>
2021-02-19 22:28:56 +0900

    Notification with group

--- src/main/kotlin/kr/co/finda/androidtemplate/feature/waistUp/WaistUpService.kt
+++ src/main/kotlin/kr/co/finda/androidtemplate/feature/waistUp/WaistUpService.kt
@@ -1,5 +1,6 @@
 package kr.co.finda.androidtemplate.feature.waistUp
 
+import com.intellij.notification.*
 import com.intellij.openapi.project.Project
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
@@ -10,6 +11,12 @@ class WaistUpService(
     private val project: Project
 ) {
 
+    private val balloonGroup = NotificationGroup(
+        "FindaTest",
+        NotificationDisplayType.BALLOON,
+        true
+    )
+
     init {
         startNotification()
     }
@@ -17,8 +24,17 @@ class WaistUpService(
     private fun startNotification() {
         GlobalScope.launch(Dispatchers.IO) {
             while (true) {
-                WaistUpNotification().notify(project)
-                delay(10000)
+                val notification = balloonGroup.createNotification(
+                    title = "허리펴!",
+                    content = "Finda 플러그인은 당신의 건강을 책임집니다",
+                    type = NotificationType.WARNING
+                )
+                notification.notify(project)
+
+                delay(3000)
+                notification.expire()
+
+                delay(8000)
             }
         }
     }
