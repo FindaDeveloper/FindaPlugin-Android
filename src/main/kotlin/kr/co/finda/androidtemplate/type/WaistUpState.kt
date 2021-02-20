9dce7254c915e53562880b0a4e7bcab9a131d1e5
kimdohun <kimdohun0104@gmail.com>
2021-02-20 18:52:00 +0900

    배포를 위한 버전업

--- src/main/kotlin/kr/co/finda/androidtemplate/type/WaistUpState.kt
+++ src/main/kotlin/kr/co/finda/androidtemplate/type/WaistUpState.kt
@@ -2,6 +2,6 @@ package kr.co.finda.androidtemplate.type
 
 data class WaistUpState(
     var isEnabled: Boolean = true,
-    var hideDelay: Long = 2000,
-    var waitDelay: Long = 2000
+    var hideDelay: Long = 4000,
+    var waitDelay: Long = 10000
 )
