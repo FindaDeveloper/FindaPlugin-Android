6a812ca2c8077addd410304acac8d12d743eb75d
kimdohun <kimdohun0104@gmail.com>
2021-03-30 12:04:38 +0900

    .

--- src/main/kotlin/kr/co/finda/androidtemplate/feature/showLayoutBound/action/ShowLayoutBoundsAction.kt
+++ src/main/kotlin/kr/co/finda/androidtemplate/feature/showLayoutBound/action/ShowLayoutBoundsAction.kt
@@ -2,9 +2,10 @@ package kr.co.finda.androidtemplate.feature.showLayoutBound.action
 
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
+import org.jetbrains.android.sdk.AndroidSdkUtils
 
 class ShowLayoutBoundsAction : AnAction() {
     override fun actionPerformed(e: AnActionEvent) {
-
+        AndroidSdkUtils.getDebugBridge(e.project!!)
     }
 }
