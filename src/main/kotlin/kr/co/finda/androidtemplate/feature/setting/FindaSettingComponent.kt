bea5a4f0e03146962af709962f5b82c086c5dc21
kimdohun <kimdohun0104@gmail.com>
2021-04-27 08:52:58 +0900

    [UPDATE] 허리펴! 기능 persistence 데이터 저장 수정

--- src/main/kotlin/kr/co/finda/androidtemplate/feature/setting/FindaSettingComponent.kt
+++ src/main/kotlin/kr/co/finda/androidtemplate/feature/setting/FindaSettingComponent.kt
@@ -1,32 +1,30 @@
 package kr.co.finda.androidtemplate.feature.setting
 
-import com.intellij.ui.components.CheckBox
 import com.intellij.ui.components.JBCheckBox
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBTextField
-import com.intellij.ui.layout.panel
 import com.intellij.util.ui.FormBuilder
-import kr.co.finda.androidtemplate.type.WaistUpState
+import kr.co.finda.androidtemplate.feature.waistUp.WaistUpStateComponent
 import javax.swing.JPanel
 
 class FindaSettingComponent(
-    state: WaistUpState
+    state: WaistUpStateComponent
 ) {
 
-    val hideDelayTextField: JBTextField = JBTextField()
-    val waitDelayTextField: JBTextField = JBTextField()
+    val displayTimeTextField: JBTextField = JBTextField()
+    val waitTimeTextField: JBTextField = JBTextField()
     val waistUpCheckBox: JBCheckBox = JBCheckBox("허리펴! 알림 활성화")
 
     val panel: JPanel = FormBuilder.createFormBuilder()
         .addComponent(JBLabel("허리펴! 설정"))
-        .addLabeledComponent("숨김 시간", hideDelayTextField)
-        .addLabeledComponent("다음 허리펴! 까지 시간", waitDelayTextField)
+        .addLabeledComponent("다이얼로그 노출 시간", displayTimeTextField)
+        .addLabeledComponent("다음 노출까지 시간", waitTimeTextField)
         .addComponent(waistUpCheckBox)
         .panel
 
     init {
-        hideDelayTextField.text = state.hideDelay.toString()
-        waitDelayTextField.text = state.waitDelay.toString()
+        displayTimeTextField.text = state.dialogDisplayTime.toString()
+        waitTimeTextField.text = state.dialogWaitTime.toString()
         waistUpCheckBox.isSelected = state.isEnabled
     }
 }
