f38f80a41ad462c73b84b26f28d0603c4b2111c7
kimdohun <kimdohun0104@gmail.com>
2021-09-30 13:03:04 +0900

    show overdraw 리팩토링

---
+++ src/main/kotlin/kr/co/finda/androidtemplate/util/DeviceUtil.kt
@@ -0,0 +1,47 @@
+package kr.co.finda.androidtemplate.util
+
+import com.android.ddmlib.IDevice
+import com.android.ddmlib.MultiLineReceiver
+import com.android.ddmlib.NullOutputReceiver
+import com.intellij.openapi.project.Project
+import org.jetbrains.android.sdk.AndroidSdkUtils
+
+object DeviceUtil {
+
+    private const val COMMAND_GET_OVERDRAW_PROPERTY = "getprop debug.hwui.overdraw"
+    private const val COMMAND_SET_OVERDRAW_PROPERTY =
+        "setprop debug.hwui.overdraw %s ; service call activity 1599295570"
+
+    fun getDebugDevices(project: Project): List<IDevice> {
+        return AndroidSdkUtils.getDebugBridge(project)?.devices?.toList() ?: emptyList()
+    }
+
+    fun toggleShowOverdrawSetting(devices: List<IDevice>) {
+        devices[0].executeShellCommand(COMMAND_GET_OVERDRAW_PROPERTY, generateMultilineReceiver {
+            val firstLine = it.firstOrNull()
+            val isShowing = firstLine?.contains("show") == true
+            devices.forEach { device ->
+                device.executeShellCommand(
+                    COMMAND_SET_OVERDRAW_PROPERTY.format(if (isShowing) "false" else "show"),
+                    NullOutputReceiver()
+                )
+            }
+        })
+    }
+
+    private fun generateMultilineReceiver(
+        onProcessNewLine: (lines: Array<out String>) -> Unit
+    ) = object : MultiLineReceiver() {
+        var cancelled = false
+
+        override fun isCancelled(): Boolean {
+            return cancelled
+        }
+
+        override fun processNewLines(lines: Array<out String>?) {
+            onProcessNewLine(lines ?: emptyArray())
+            cancelled = true
+        }
+
+    }
+}
